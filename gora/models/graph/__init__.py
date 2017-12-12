ff96d9e607f59a81bd502f67d52e8d64a2015cb8
Pedro Guzman <pguzmanb498@ulacit.ed.cr>
2017-12-12 17:09:15 +0000

    Created the models

--- gora/models/graph/__init__.py
+++ gora/models/graph/__init__.py
@@ -41,12 +41,15 @@ class User(StructuredNode):
     password = StringProperty(required=True)
     is_active = BooleanProperty(required=True)
     
+    instruments = StringProperty(required=True)
+    
     is_anonymous = False
     is_authenticated = True
     
     # RELATIONS
     groups = RelationshipTo('Group', 'IS_MEMBER_OF')
-    courses = RelationshipTo('Course', 'TEACHES')
+    bands = RelationshipTo('Band', "IS_MEMBER_OF")
+    
 
     # --------------------------------------------------------------------------
     # DGET ID
@@ -126,146 +129,45 @@ class Group(StructuredNode):
             if not prop.startswith('__'):
                 output[prop] = getattr(self, prop)
         return output
-        
+  
     
 # ##############################################################################
 # BUSINESS OBJECTS
 # ##############################################################################
-# (year, month, day)
-
-class LegalGuardian(StructuredNode):
-
-    # Attributes
-    personal_id = StringProperty(unique_index=True, required=True)
-    name = StringProperty(required=True)
-    last_name = StringProperty(required=True)
-    second_last_name = StringProperty(required=True)
-    phone_number = StringProperty(required=True, index=True)
-    email = StringProperty()
-    address = StringProperty()
-
-    # Edges
-    dependents = RelationshipTo('Student', 'IS_RESPONSIBLE_FOR')
-
 
 # ------------------------------------------------------------------------------
-# CLASS STUDENT
-# ------------------------------------------------------------------------------
-class Student(StructuredNode):
-
-    # Attributes
-    student_id = StringProperty(unique_index=True, required=True)
-    personal_id = StringProperty(unique_index=True, required=True)
-    date_of_birth = DateProperty(required=True)
-    name = StringProperty(required=True, index=True)
-    last_name = StringProperty(required=True, index=True)
-    second_last_name = StringProperty(required=True)
-    education_level = StringProperty(required=True, index=True)
-    education_level_year = StringProperty(required=True, index=True)
-    active = BooleanProperty(required=True)
-    section = IntegerProperty(required=True)
-
-    # Edges
-    legal_guardians = RelationshipTo('LegalGuardian', 'IS_DEPENDENT_OF')
-    courses = RelationshipTo('Course', 'IS_TAKING')
-    past_courses = RelationshipTo('Course', 'TOOK')
-
-    # --------------------------------------------------------------------------
-    # DICTIONARY PROPERTY
-    # --------------------------------------------------------------------------
-    @property
-    def dictionary(self):
-        """
-            Gets a dictionary representation of the current instance of Student
-            :return: 
-        """
-        output = {}
-        for prop in self.__dict__.keys():
-            if not prop.startswith('__'):
-                output[prop] = getattr(prop)
-        return output
-
-    # --------------------------------------------------------------------------
-    # SET DATE OF BIRTH
-    # --------------------------------------------------------------------------
-    def set_date_of_birth(self, day, month, year):
-        """
-            Sets the date object in Student class
-            
-            :param day: The day
-            :param month: The month
-            :param year: The year
-            :return: True if well formatted and valid date provided. False if not.
-            
-        """
-        try:
-            self.date_of_birth = date(year, month, day)
-            return True
-        except Exception as ex:
-            # TODO good exception handling!!!
-            print(ex)
-            return False
-            
-
-# ------------------------------------------------------------------------------
-# CLASS COURSE
-# ------------------------------------------------------------------------------
-class Course(StructuredNode):
+# CLASS BAND
+# ------------------------------------------------------------------------------       
+class Band(StructuredNode):
     
-    # ATTRIBUTES ---------------------------------------------------------------
-    course_id = StringProperty(unique_index=True, required=True)
-    title = StringProperty(required=True, index=True)
+    # PROPERTIES
+    band_id = StringProperty(unique_index=True, required=True)
+    name = StringProperty(required=True, unique_index=True)   
+    genres = StringProperty(required=True)  
+    logo_url = StringProperty() 
+    description = StringProperty(required=True) 
+    founded = DateProperty(required=True)
+    # Edges
+    members = RelationshipTo('User', 'HAS_MEMBER')
+    rehearsals = RelationshipTo('Rehearsal', 'HAS_SCHEDULED')
+    
+# ------------------------------------------------------------------------------
+# CLASS BAND
+# ------------------------------------------------------------------------------       
+class Rehearsal(StructuredNode):
+    
+    # PROPERTIES
+    rehearsal_id = StringProperty(unique_index=True, required=True)
+    date = DateProperty(required=True, index=True)
+    start_hour = IntegerProperty(required=True)
+    start_minute = IntegerProperty(required=True)
+    end_hour = IntegerProperty(required=True)
+    end_minute = IntegerProperty(required=True)
+    place= StringProperty(required=True)
     description = StringProperty(required=True)
-    year = IntegerProperty(required=True, index=True)
-    education_level_year = StringProperty(required=True, index=True)
     
-    # RELATIONS ----------------------------------------------------------------
-    taught_by = RelationshipTo('User', 'IS_TAUGHT_BY')
-    students = RelationshipTo('Student', 'IS_BEING_TAKEN_BY')
-    period = RelationshipTo('Period', 'IS_GIVEN_DURING', cardinality=One)
- 
- 
-# ------------------------------------------------------------------------------
-# CLASS COURSE
-# ------------------------------------------------------------------------------    
-class Grade(StructuredNode):
-    
-    # ATTRIBUTES ---------------------------------------------------------------
-    passing = BooleanProperty(required=True)
-    total_points = FloatProperty(required=True)
-    value_percentage = FloatProperty(required=True)
-    date = DateProperty(required=True)
-    
-    # RELATIONS ----------------------------------------------------------------
-    student = RelationshipTo('Student', 'WAS_OBTAINED_BY', cardinality=One)
-    course = RelationshipTo('Course', 'WAS_OBTAINED_IN', cardinality=One)
-    period = RelationshipTo('Period', 'WAS_OBTAINED_DURING', cardinality=One)
-    
-    
-# ------------------------------------------------------------------------------
-# CLASS COURSE
-# ------------------------------------------------------------------------------    
-class Period(StructuredNode):
-    
-    # ATRIBUTES ----------------------------------------------------------------
-    
-    year = IntegerProperty(index=True, required=True)
-    denominator = IntegerProperty(index=True, required=True)
-    number = IntegerProperty(index=True, required=True)
-
-    # RELATIONS ----------------------------------------------------------------
-    grades = RelationshipTo('Grade', 'HAS')
-    courses = RelationshipTo('Course', 'HAS')
-    
-    # --------------------------------------------------------------------------
-    # METHOD STATE IS VALID
-    # --------------------------------------------------------------------------
-    def state_is_valid(self):
-        if self.year < 2016:
-            return False
-        if self.number > self.denominator:
-            return False
-        return True
+    # Edges
+    band = RelationshipTo('Band', 'SCHEDULED_FOR', cardinality=One)
 
 # ##############################################################################
 # TRANSACTIONAL OBJECTS
--- update_model.sh
+++ update_model.sh
@@ -1,3 +1,3 @@
-cd thrive/models
-neomodel_install_labels graph/__init__.py graph --db bolt://neo4j:Awsx1Sedc2Drfv34@graph.subvertic.com:7687
+cd gora/models
+neomodel_install_labels graph/__init__.py graph --db bolt://gora:b.d8zPYuJPq8Tw.TF0PE2Dj5xjOX6ez@hobby-nfambjifjhecgbkegfhggjal.dbs.graphenedb.com:24786
 cd ../..
